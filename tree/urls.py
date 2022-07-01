fd61170ca413b2cadf91cf5b2b5f99ea331ad119
vseop <ivpython@mail.ru>
2022-07-01 11:25:13 +0300

    update readmy

--- tree/urls.py
+++ tree/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TreeViewSet
 
 router = DefaultRouter()
-router.register(r'three', TreeViewSet)
+router.register(r'tree', TreeViewSet)
 urlpatterns = [
     path('', include(router.urls))
 ]
